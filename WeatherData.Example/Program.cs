9e770214382cad3bad543bebbf05538bed8b4b71
Will Holmes <willholmes97@icloud.com>
2019-06-19 19:18:37 +0100

    Added a sample implementation project

---
+++ WeatherData.Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherData.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GetWeather().GetAwaiter().GetResult();
+        }
+
+        static async Task GetWeather() {
+            bool exitApp = false;
+            IWeatherDataService weatherDataService = new WeatherDataService();
+
+            while (!exitApp)
+            {
+                Console.WriteLine("What city would you like to get the weather for?");
+                string location = Console.ReadLine();
+
+                var response = await weatherDataService.GetTodaysWeatherDataByCity(location);
+
+                var weather = response
+                    .ConsolidatedWeather
+                    .OrderByDescending(x => x.Predictability)
+                    .First();
+                
+                Console.WriteLine($"The minimum temperature for {response.Title} is {Math.Round(weather.MinTemp)}c");
+                Console.WriteLine($"The maxiumum temperature for {response.Title} is {Math.Round(weather.MaxTemp)}c");
+                Console.WriteLine($"The most likely temperature for {response.Title} is {Math.Round(weather.TheTemp)}c");
+
+                Console.WriteLine("Would you like to get the weather for another city? Y/N");
+                string anotherCity = Console.ReadLine().ToLower();
+                while (!(anotherCity == "n" || anotherCity == "y"))
+                {
+                    Console.WriteLine("Please enter y or n again!");
+                    anotherCity = Console.ReadLine().ToLower();
+                }
+
+                exitApp = anotherCity.Equals("n");
+            }
+        }
+    }
+}
