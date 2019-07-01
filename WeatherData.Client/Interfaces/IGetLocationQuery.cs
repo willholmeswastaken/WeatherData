ef5a991d6c2f3e376f0d3422d8cbcfd24a56e0d2
Will Holmes <willholmes97@icloud.com>
2019-07-01 22:57:26 +0100

    Added relevant method commenting on interfaces

--- WeatherData.Client/Interfaces/IGetLocationQuery.cs
+++ WeatherData.Client/Interfaces/IGetLocationQuery.cs
@@ -5,10 +5,23 @@ using WeatherData.Models.Location;
 
 namespace WeatherData.Client.Interfaces
 {
+    /// <summary>
+    /// Get the information of a location through either, city or latlon.
+    /// </summary>
     public interface IGetLocationQuery : IWebQuery
     {
+        /// <summary>
+        /// Gets the location by city.
+        /// </summary>
+        /// <returns>The location by city.</returns>
+        /// <param name="city">City name.</param>
         Task<LocationResult> GetLocationByCity(string city);
 
+        /// <summary>
+        /// Gets the location by lat lon.
+        /// </summary>
+        /// <returns>The location by lat lon.</returns>
+        /// <param name="position">Contains lat and lon properties.</param>
         Task<LocationResult> GetLocationByLatLon(Position position);
     }
 }
--- WeatherData.Client/Interfaces/IGetWeatherQuery.cs
+++ WeatherData.Client/Interfaces/IGetWeatherQuery.cs
@@ -4,10 +4,24 @@ using WeatherData.Models.Weather;
 
 namespace WeatherData.Client.Interfaces
 {
+    /// <summary>
+    /// Get the weather information of a location with the Where on earth Id (woeId)
+    /// </summary>
     public interface IGetWeatherQuery : IWebQuery
     {
+        /// <summary>
+        /// Gets the todays weather by location.
+        /// </summary>
+        /// <returns>The todays weather by location.</returns>
+        /// <param name="woeId">Where on earth Id.</param>
         Task<WeatherResult> GetTodaysWeatherByLocation(string woeId);
 
+        /// <summary>
+        /// Gets the weather for date by location.
+        /// </summary>
+        /// <returns>The weather for date by location.</returns>
+        /// <param name="date">Target date.</param>
+        /// <param name="woeId">Where on earth Id.</param>
         Task<WeatherResult> GetWeatherForDateByLocation(DateTime date, string woeId);
     }
 }
